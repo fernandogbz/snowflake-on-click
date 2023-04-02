aaf37956db3fb474face6f2ac1b6b9d98706b103
Fernando Jimenez <fgjv06@gmail.com>
2023-04-02 06:26:21 -0400

    JS - set position of snowflake to mouse pointer's position

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ document.addEventListener("click", (e) =>{
   // Create span for snowflake
   var snowflake = document.createElement("span");
   snowflake.classList.add("snowflake");
+
+  // Set position of snowflake to mouse pointer's position
+  snowflake.style.left = e.offsetX + "px";
+  snowflake.style.top = e.offsetY + "px";
 });
